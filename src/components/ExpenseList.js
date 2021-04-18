088cc4c370d37e5a649b5bb2d965c9b1da80bf83
Zoe <zoe.steel@xneelo.com>
2021-04-17 23:38:58 -0700

    Enzyme tests for ExpenseList and ExpenseListItem

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,12 +4,18 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
   <div>
+    {props.expenses.length === 0 ? (
+      <p>No Expenses.</p>
+      ) : (
+        props.expenses.map((expense) => {
+          return <ExpenseListItem key={expense.id} {...expense} />;
+        })
+      )
+    }
     <h1>ExpenseList</h1>
-    {props.expenses.map((expense) => {
-      return <ExpenseListItem key={expense.id} {...expense} />;
-    })}
+    {}
   </div>
 );
 
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
+export const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
